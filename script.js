bc7b51ed2d58209dd7783bcf798e265e0d2b8a39
sumana10 <sumanaonlyme@gmail.com>
2022-03-21 15:18:54 +0530

    finish-project

--- script.js
+++ script.js
@@ -1,28 +1,69 @@
 const form = document.getElementById('form');
 
 
-
-
 form.addEventListener('submit', (e)=>{
   e.preventDefault();
 
-  const first = form['firstname'].value;
+const first = form['firstname'].value;
 const last = form['lastname'].value;
 const email = form['email'].value;
 const password =form['password'].value;
 
 
 if(first === ""){
+addErrorTo('firstname', 'First Name is required')
+}else{
+  removeErrorFrom('firstname')
+}
+if(last === ""){
+addErrorTo('lastname', 'Last Name is required')
+}else{
+  removeErrorFrom('lastname')
+}
+if(email === ""){
+addErrorTo('email', 'Email is required')
+}else if(!isValid(email)){
 
-  const small = form['firstname'].parentNode.querySelector('small');
+  addErrorTo('email', 'Email is not valid')
 
-  small.innerText = 'First Name is required'
+}
+else{
+  removeErrorFrom('email')
+}
+if(password === ""){
+addErrorTo('password', 'Pasword is required')
+}else{
+  removeErrorFrom('password')
+}
+
+})
+
+function addErrorTo(field, message){
+
+  const formControl = form[field].parentNode;
+
+  formControl.classList.add('error')
+
+  const small = formControl.querySelector('small');
+
+  small.innerText = message
 
   small.style.opacity = '1'
 
 }
+function removeErrorFrom(field){
 
-  
+  const formControl = form[field].parentNode;
 
-})
+  formControl.classList.remove('error')
+  const small = form[field].parentNode.querySelector('small');
+
+  small.style.opacity = '0';
+
+}
+
+function isValid(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
 
